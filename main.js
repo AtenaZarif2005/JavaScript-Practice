a0c07a66c0da34f56da9576179303e9277ca6c91
AtenaZarif2005 <atenazarif2005@gmail.com>
2026-09-12 17:53:26 +0330

    Restore JavaScript code

---
+++ main.js
@@ -0,0 +1,71 @@
+//تمرین اول انواع متغییر
+
+// const nameOfShopping="digiKala";
+// let name="airPods";
+// name="Ipads";
+// let price=100;
+// let price=1000;
+// let stockStatus=true;
+// console.log(nameOfShopping);
+// console.log(name);
+// console.log(price);
+// console.log(stockStatus);
+// console.log(typeof nameOfShopping);
+// console.log(typeof name);
+// console.log(typeof price);
+// console.log(typeof stockStatus);
+//_____________________________________________________________________________________________________________________
+
+// تمرین دوم رشته ها
+
+// let width="400px";
+// console.log(parseInt(width));
+// let massage=`new price is ${parseInt(width)+50}px`;
+// console.log(massage);
+// ____________________________________________________________________________________________________________________
+
+//تمرین سوم اطلاعات کاربر
+
+// let numb1 = +prompt("number 1");
+// console.log(numb1);
+// let numb2 = +prompt("number 2");
+// console.log(numb2);
+// let numb3 = alert(numb1+numb2);
+// console.log(numb3);
+//____________________________________________________________________________________________________________________
+
+// متد confirm تمرین  
+
+// let isSure = confirm("Are you sure you wanna delete your account?");
+// console.log(isSure);
+
+//____________________________________________________________________________________________________________________
+
+// تمرین عملگرهای محاسباتی
+
+// let numb1 = 20;
+// let numb2 = 5;
+// console.log(numb1+numb2);
+// console.log(numb1-numb2);
+// console.log(numb1%numb2);
+// console.log(numb1/numb2);
+// console.log(numb1**numb2);
+
+//____________________________________________________________________________________________________________________
+
+//تمرین عملگرهای مقایسه ای
+
+// let personWeight1 = prompt("plz enter ur weight");
+// let personHeight1 = prompt("plz enter ur height");
+// let personWeight2 = prompt("plz enter ur weight");
+// let personHeight2 = prompt("plz enter ur height");
+// let person1BMI = personWeight1 / (personHeight1**2);
+// let person2BMI = personWeight2 / (personHeight2**2);
+// console.log(person1BMI);
+// console.log(person2BMI);
+// console.log(person1BMI===person2BMI);
+// console.log(person1BMI>=person2BMI);
+
+//____________________________________________________________________________________________________________________
+
+
